bc67dd768527332decea21df38ab8d364821ca39
thangdduong <19522195@gm.uit.edu.vn>
2022-11-06 01:02:52 +0700

    load bm25 pickle file

--- submit.py
+++ submit.py
@@ -2,7 +2,6 @@ import pickle
 import json
 import argparse
 
-from utils.retrieval import train_context_retrieval
 from utils.retrieval import context_retrieval
 from utils.answering import QA
 
@@ -10,23 +9,26 @@ from utils.answering import QA
 def main():
     parser = argparse.ArgumentParser(description='Some arguments for submission file')
 
-    parser.add_argument('--test_path', type=str, default='./dataset/zac2022_testa_only_question.json',
+    parser.add_argument('--test_path', type=str, default='./dataset/train_publicTest/zac2022_testa_only_question.json',
                         help='The test dataset json file')
-    parser.add_argument('--pickle_path', type=str, default='./dataset/corpus.pkl',
+    parser.add_argument('--corpus_pkl_path', type=str, default='./dataset/corpus.pkl',
                         help='Path to created corpus')
+    parser.add_argument('--bm25_pkl_path', type=str, default='./dataset/bm25.pkl',
+                        help='Path to created bm25')
     parser.add_argument('--submit_filename', type=str, default='submission.json',
                         help='Filename of final submission json file')
     
     args = parser.parse_args()
 
-    with open(args.corpus_path, 'rb') as f:
+    with open(args.corpus_pkl_path, 'rb') as f:
         dataset = pickle.load(f)
 
     corpus = [
         record['text'] for record in dataset
     ]
 
-    bm25 = train_context_retrieval(corpus)
+    with open(args.bm25_pkl_path, 'rb') as f:
+        bm25 = pickle.load(f)
 
     public_test_path = args.test_path
     records = []
