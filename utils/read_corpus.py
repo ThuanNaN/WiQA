185244fdeb81e390efc51b0a0e5dbc0be5b9abf6
thangdduong <19522195@gm.uit.edu.vn>
2022-11-06 01:36:57 +0700

    create bm25 in retrieval.py

--- utils/read_corpus.py
+++ utils/read_corpus.py
@@ -2,8 +2,6 @@ import json
 import pickle
 import argparse
 
-from utils.retrieval import train_context_retrieval
-
 def main():
     parser = argparse.ArgumentParser(description='Some arguments for creating corpus')
 
@@ -17,12 +15,6 @@ def main():
     with open(args.corpus_path, 'r', encoding='utf8') as f:
         data = f.read()
         dataset = json.loads(data)
-
-    corpus = [
-        record['text'] for record in dataset
-    ]
-
-    train_context_retrieval(corpus)
 
     with open(args.save_path, 'wb') as f:
         pickle.dump(dataset, f)
--- utils/retrieval.py
+++ utils/retrieval.py
@@ -12,14 +12,9 @@ def clean_text(text):
 
     return text
 
-def train_context_retrieval(corpus, save_bm25=True):
+def train_context_retrieval(corpus):
   tokenized_corpus = [clean_text(doc).split(" ") for doc in corpus]
   bm25 = BM25Okapi(tokenized_corpus)
-
-  if save_bm25 == True:
-    save_path = '../dataset/bm25.pkl'
-    with open(save_path, 'wb') as f:
-      pickle.dump(bm25, f)
 
   return bm25
 
@@ -29,3 +24,21 @@ def context_retrieval(question, corpus, bm25, top_k=1):
   rank_lst = bm25.get_top_n(tokenized_query, corpus, n=top_k)
 
   return rank_lst[0]
+
+def main():
+  corpus_pkl_path = './dataset/corpus.pkl'
+  save_path = '../dataset/bm25.pkl'
+  with open(corpus_pkl_path, 'rb') as f:
+    dataset = pickle.load(f)
+
+  corpus = [
+      record['text'] for record in dataset
+  ]
+
+  bm25 = train_context_retrieval(corpus)
+
+  with open(save_path, 'wb') as f:
+    pickle.dump(bm25, f)
+
+if __name__ == '__main__':
+  main()
