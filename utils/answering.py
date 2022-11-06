8416d50fcb87a5f749056831dbab1780e19eafd2
ThuanNaN <duongthuan1445@gmail.com>
2022-11-06 15:11:03 +0700

    gpu runtime

--- utils/answering.py
+++ utils/answering.py
@@ -1,8 +1,9 @@
-from models.nguyenvulebinh_qa.infer import data_collator, extract_answer, tokenize_function_2
+from models.nguyenvulebinh_qa.infer import data_collator_2device, extract_answer, tokenize_function_2, data_collator
 from models.nguyenvulebinh_qa.model.mrc_model import MRCQuestionAnswering
 from transformers import AutoTokenizer
 import nltk
 
+import time
 nltk.download('punkt')
 
 device = 'cuda'
@@ -19,16 +20,23 @@ def mailong_qa(question, context):
 model_checkpoint = "nguyenvulebinh/vi-mrc-large"
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 model = MRCQuestionAnswering.from_pretrained(model_checkpoint)
+model.to(device)
 
 def nguyenvulebinh_qa(question, context):
+
   QA_input = {
       "question": question,
       "context": context
   }
-  
-  inputs = [tokenize_function_2(QA_input, tokenizer)]
-  inputs_ids = data_collator(inputs, tokenizer)
-  outputs = model(**inputs_ids)
-  answer = extract_answer(inputs, outputs, tokenizer)
 
+
+  inputs = [tokenize_function_2(QA_input, tokenizer)]
+
+  #inputs_ids = data_collator(inputs, tokenizer)
+
+  inputs_ids = data_collator_2device(inputs, tokenizer, device)
+  
+  outputs = model(**inputs_ids)
+
+  answer = extract_answer(inputs, outputs, tokenizer)
   return answer[0]['answer']
