9760abf5b201349d2c9b872ff9864217187041a2
ThuanNaN <duongthuan1445@gmail.com>
2022-11-06 15:56:26 +0700

    add device gpu

--- test_function.py
+++ test_function.py
@@ -1,5 +1,6 @@
 
-from utils.answering import nguyenvulebinh_qa
+from utils.answering import nguyenvulebinh_qa, mailong_qa
+from utils.utils import load_model
 import time
 
 
@@ -7,8 +8,13 @@ question = "Người giàu nhất Việt Nam"
 
 relevant_doc = " Ông Phạm Nhật Vượng tiếp tục được công nhận là người giàu nhất Việt Nam với tài sản định giá 6,6 tỷ USD, đứng thứ 239 thế giới, tăng 2,3 tỷ USD so với năm ngoái."
 
+model_name = "mailong"
+device = "cpu"
+model= load_model(model_name, device)
+
+
 start = time.time()
-answer = nguyenvulebinh_qa(question, relevant_doc)
+answer = mailong_qa(model, question, relevant_doc)
 finish = time.time()- start
 
 print(answer)
--- utils/answering.py
+++ utils/answering.py
@@ -1,42 +1,30 @@
-from models.nguyenvulebinh_qa.infer import data_collator_2device, extract_answer, tokenize_function_2, data_collator
-from models.nguyenvulebinh_qa.model.mrc_model import MRCQuestionAnswering
-from transformers import AutoTokenizer
-import nltk
+from models.nguyenvulebinh_qa.infer import data_collator_2device, extract_answer, tokenize_function_2
 
-import time
-nltk.download('punkt')
-
-device = 'cuda'
-
+#---------------------------
 #### mailong model
-from models.mailong_qa.reader import Reader
-reader = Reader()
 
-def mailong_qa(question, context):  
-    answers, score = reader.getPredictions(question, [context])[0]
+def mailong_qa(model, question, context):  
+    answers, score = model["model"].getPredictions(question, [context])[0]
     return answers
+#---------------------------
 
-#####
-model_checkpoint = "nguyenvulebinh/vi-mrc-large"
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-model = MRCQuestionAnswering.from_pretrained(model_checkpoint)
-model.to(device)
 
-def nguyenvulebinh_qa(question, context):
+#---------------------------
+##### nguyenvulebinh model
+
+def nguyenvulebinh_qa(model, question, context):
 
   QA_input = {
       "question": question,
       "context": context
   }
-
-
+  tokenizer = model["tokenizer"]
+  device = model["device"]
   inputs = [tokenize_function_2(QA_input, tokenizer)]
-
-  #inputs_ids = data_collator(inputs, tokenizer)
-
   inputs_ids = data_collator_2device(inputs, tokenizer, device)
-  
-  outputs = model(**inputs_ids)
-
+  outputs = model["model"](**inputs_ids)
   answer = extract_answer(inputs, outputs, tokenizer)
   return answer[0]['answer']
+
+
+
