c431bcdd9594ef858e7f2921fcc2f8c8cb33228e
NhatTan <susannoo123@gmail.com>
2022-11-07 17:52:14 +0700

    change lucene_indexer.py location

---
+++ lucene_indexer.py
@@ -0,0 +1,20 @@
+import subprocess
+import shlex
+import yaml
+from utils import execute, load_config
+
+
+if __name__ == '__main__':    
+    lucene_indexer_cmd = """
+    python -m pyserini.index.lucene --storeContents \
+                                    --bm25.accurate \
+    """
+
+    cmd_args = shlex.split(lucene_indexer_cmd)
+    config = load_config(key="lucene")
+
+    for k, v in config.items():
+        cmd_args += [k] + [v]
+
+    for path in execute(cmd_args):
+        print(path, end="")
