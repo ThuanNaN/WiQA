bfb0ffdcfc40f79cac06d54515a497dc1a1c8a71
thuannd <duongthuan1445@gmail.com>
2022-11-15 20:52:11 +0700

    QA per sentence

--- utils/answering.py
+++ utils/answering.py
@@ -1,5 +1,6 @@
-from qa_models.nguyenvulebinh_qa.infer import data_collator_2device, extract_answer, tokenize_function_2
+from qa_models.nguyenvulebinh_qa.infer import data_collator_2device, extract_answer, tokenize_function_2, tokenize_function
 import nltk
+import numpy as np
 nltk.download('punkt')
 
 #---------------------------
@@ -16,16 +17,25 @@ def mailong_qa(model, question, context):
 
 def nguyenvulebinh_qa(model, question, context):
 
-  QA_input = {
-      "question": question,
-      "context": context
-  }
-  tokenizer = model["tokenizer"]
-  device = model["device"]
-  inputs = [tokenize_function_2(QA_input, tokenizer)]
-  inputs_ids = data_collator_2device(inputs, tokenizer, device)
-  outputs = model["model"](**inputs_ids)
-  answer = extract_answer(inputs, outputs, tokenizer)
-  return answer[0]['answer']
+    lst_context = context.split(".")
+    answers = []
+
+    for i in range(0, int(len(lst_context)/2)):
+        QA_input = {
+            "question": question,
+            "context": lst_context[i*2] + lst_context[i*2+1]
+        }
+        tokenizer = model["tokenizer"]
+        device = model["device"]
+
+        inputs = [tokenize_function(QA_input, tokenizer)]
+        inputs_ids = data_collator_2device(inputs, tokenizer, device)
+        outputs = model["model"](**inputs_ids)
+
+        answer = extract_answer(inputs, outputs, tokenizer)
+        answers.append(answer[0]['answer'])
+
+    answer_counting = np.unique(answers, return_counts=True)
+    return answer_counting[0][np.argmax(answer_counting[1])]
 
 
